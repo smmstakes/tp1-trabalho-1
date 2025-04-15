e0098465c52cb169963a263b5f17ca9a27d2b4dd
smmstakes <matheusds014@gmail.com>
2025-04-15 08:16:37 -0300

    Ajustando a organização das classes

--- trabalho-1/dominios.hpp
+++ trabalho-1/dominios.hpp
@@ -41,47 +41,11 @@ class CPF {
 };
 
 
-class Senha {
-    private:
-        std::string senha;
-        static const std::regex PADRAO_SENHA;
-        void validar(std::string senha);
-
-    public:
-        void set(std::string senha);
-        std::string get() const {return senha;}
-};
-
-class Dinheiro {
-    private :
-        double dinheiro;
-        static const double DINHEIRO_MIN;
-        static const double DINHEIRO_MAX;
-        void validar(double dinheiro);
-    public:
-        double get() const {return dinheiro};
-        void set(double dinheiro);
-// Criado por Pedro Vale - 231038733
-};
-
-class Quantidade {
-    private:
-        int quantidade;
-        static const int VALOR_MIN= 1;
-        static const int VALOR_MAX=1000000;
-        void validar(int quantidade);
-    public:
-        int get() const {return quantidade};
-        void set(int quantidade);
-// Criada por Pedro Vale - 231038733
-};
-
 class Data {
     private:
-        static const std::regex FORMATO;
         std::string data;
-        bool ehAnoBissexto(int ano) const{
-            // Combinamos metodos de retorno direto na classe? inline?
+        static const std::regex FORMATO;
+        bool ehAnoBissexto(int ano) const {
             return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
         };
         void validar(const std::string& data);
@@ -91,15 +55,54 @@ class Data {
         std::string get() const {return data;}
 };
 
+
 class Perfil {
     private:
-    std::string perfil;
-    static const std::regex PADRAO_PERFIL;
-    void valida(std::string perfil);
+        std::string perfil;
+        static const std::regex PADRAO_PERFIL;
+        void validar(std::string perfil);
 
     public:
-    void set(std::string perfil);
-    std::string get() const{return perfil;}
+        void set(std::string perfil);
+        std::string get() const {return perfil;}
+};
+
+
+class Dinheiro {
+    private :
+        double dinheiro;
+        static const double DINHEIRO_MIN;
+        static const double DINHEIRO_MAX;
+        void validar(double dinheiro);
+
+    public:
+        void set(double dinheiro);
+        double get() const {return dinheiro;}
+};
+
+
+class Quantidade {
+    private:
+        int quantidade;
+        static const int VALOR_MIN = 1;
+        static const int VALOR_MAX = 1000000;
+        void validar(int quantidade);
+
+    public:
+        void set(int quantidade);
+        int get() const {return quantidade;}
+};
+
+
+class Senha {
+    private:
+        std::string senha;
+        static const std::regex PADRAO_SENHA;
+        void validar(std::string senha);
+
+    public:
+        void set(std::string senha);
+        std::string get() const {return senha;}
 };
 
 #endif // DOMINIOS_HPP_INCLUDED
