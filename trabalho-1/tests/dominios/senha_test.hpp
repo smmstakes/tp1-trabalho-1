8a9b5b3556f01a50b3e0584332860d12f43ccd2b
smmstakes <matheusds014@gmail.com>
2025-04-23 09:07:05 -0300

    Adicionando arquivos de teste do domínio Senha

---
+++ trabalho-1/tests/dominios/senha_test.hpp
@@ -0,0 +1,34 @@
+#ifndef CPF_TEST_HPP_INCLUDED
+#define CPF_TEST_HPP_INCLUDED
+
+#include <string>
+
+#include "../../src/dominios/dominios.hpp"
+
+class TUSenha {
+    private:
+        const static std::string VALOR_VALIDO;
+        const static std::string DIGITO_INVALIDO;
+        const static std::string TAMANHO_INVALIDO;
+        const static std::string CRITERIO_MINIMO_INVALIDO;
+        const static std::string CARACTER_DUPLICADO_INVALIDO;
+
+        Senha *senha;
+        int estado;
+
+        void setUp();
+        void tearDown();
+        void testarCenarioValido();
+        void testarDigitoInvalido();
+        void testarTamanhoInvalido();
+        void testarCriterioMinimoInvalido();
+        void testarCaracterDuplicadoInvalido();
+
+    public:
+        const static int SUCESSO = 1;
+        const static int FALHA = -1;
+
+        int run();
+};
+
+#endif // CPF_TEST_HPP_INCLUDED
