b7e59e8c2e00d31653dae51a5e06454c409d65a1
suyanne <suyannesara17@gmail.com>
2025-04-23 09:00:33 -0300

    add testes a main

--- trabalho-1/main.cpp
+++ trabalho-1/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include "./tests/dominios/data_test.hpp"
+#include "./tests/dominios/codigo_test.hpp"
+#include "./tests/dominios/codigo_negociacao_test.hpp"
+#include "./tests/dominios/cpf_test.hpp"
 
 using namespace std;
 
 int main() {
+    TUCodigo tCodigo;
+    tCodigo.run();
 
-    cout << "Hello World!" << endl;
+    TUCodigoNegociacao tCodigoNegociacao;
+    tCodigoNegociacao.run();
+
+    TUCPF tCPF;
+    tCPF.run();
+
+    TUData tData;
+    tData.run();
 
     return 0;
 }
