d1627db068d5e0d78c6e2b26a05919fe37f7384b
suyanne <suyannesara17@gmail.com>
2025-06-11 18:26:05 -0300

    adjust compile

--- trabalho-1/main.cpp
+++ trabalho-1/main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
 #include "tests/utils/executor_de_testes.hpp"
 #include <iostream>
-#include "./src/servico/MSInvestimentos/MSInvestimentos.hpp"  // Classe de serviço que implementa a lógica de negócios
-#include "./src/apresentacao/MAInvestimentos/CntrlIAInvestimentos.hpp"  // Controlador de interface de usuário para investimentos
+#include "./src/servico/MSInvestimentos/MSInvestimentos.hpp" 
+#include "./src/apresentacao/MAInvestimentos/CntrlIAInvestimentos.hpp" 
+
 
 int main() {
-    MSInvestimentos servicoInvestimentos;
-
-    std::string codigo = "00001";
-    std::string nome = "Carteira T2";
-    std::string perfil = "Moderado";
-
     try {
         CntrlIAInvestimentos controladorInvestimentos;
         controladorInvestimentos.executar();
@@ -21,14 +16,3 @@ int main() {
     return 0;
 }
 
-
-// int main() {
-//     try {
-//         ExecutorDeTestes::executarTodos();
-//     } catch (const std::string& nomeTipo) {
-//         std::cerr << "Erro: teste '" << nomeTipo << "' falhou. Encerrando execução." << std::endl;
-//         return 1;
-//     }
-
-//     return 0;
-// }
