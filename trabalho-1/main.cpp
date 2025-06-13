067a7da7855148e35476dfbab4f604ee34aba4bc
smmstakes <matheusds014@gmail.com>
2025-06-13 19:48:02 -0300

    Limpando o main.cpp

--- trabalho-1/main.cpp
+++ trabalho-1/main.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
-#include "tests/utils/executor_de_testes.hpp"
-#include <iostream>
-#include "./src/servico/MSInvestimentos/MSInvestimentos.hpp" 
-#include "./src/apresentacao/MAInvestimentos/CntrlIAInvestimentos.hpp" 
 
+using namespace std;
 
 int main() {
-    try {
-        CntrlIAInvestimentos controladorInvestimentos;
-        controladorInvestimentos.executar();
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-    }
+    cout << "Hello, World!\n";
 
     return 0;
 }
